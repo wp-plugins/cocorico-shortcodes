ebb8da48ec6fbd442b937d34ebabc33b6ad9ca21
themesdefrance <themesdefrance@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-02 06:45:53 +0000

    launch v1.0.0 cocorico shortcodes
    git-svn-id: https://plugins.svn.wordpress.org/cocorico-shortcodes/tags/1.0.0@1000370 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/cocorico-shortcodes-editor-langs.php
@@ -0,0 +1,177 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+if ( ! class_exists( '_WP_Editors' ) )
+    require( ABSPATH . WPINC . '/class-wp-editor.php' );
+
+function coco_shortcodes_tinymce_translation() {
+    $strings = array(
+    	// Column shortcode strings
+    	'column'				=> _x('Column', 'tinymce button label', 'cocoshortcodes'),
+        'column_add'			=> _x('Add a column', 'tinymce popup title', 'cocoshortcodes'),
+        
+        'column_size'			=> _x('Size', 'tinymce popup attribute label', 'cocoshortcodes'),
+        'column_one_half'		=> _x('one_half', 'shortcode attribute value', 'cocoshortcodes'),
+        'column_one_third'		=> _x('one_third', 'shortcode attribute value', 'cocoshortcodes'),
+        'column_two_thirds'		=> _x('two_thirds', 'shortcode attribute value', 'cocoshortcodes'),
+        'column_one_fourth'		=> _x('one_fourth', 'shortcode attribute value', 'cocoshortcodes'),
+        'column_three_fourths'	=> _x('three_fourths', 'shortcode attribute value', 'cocoshortcodes'),
+        
+        'column_position'		=> _x('Position', 'tinymce popup attribute label', 'cocoshortcodes'),
+        'column_first'			=> _x('First', 'tinymce popup select label', 'cocoshortcodes'),
+        'column_first_value'	=> _x('first', 'shortcode attribute value', 'cocoshortcodes'),
+        'column_middle'			=> _x('Middle', 'tinymce popup select label', 'cocoshortcodes'),
+        'column_middle_value'	=> _x('middle', 'shortcode attribute value', 'cocoshortcodes'),
+        'column_last'			=> _x('Last', 'tinymce popup select label', 'cocoshortcodes'),
+        'column_last_value'		=> _x('last', 'shortcode attribute value', 'cocoshortcodes'),
+        
+        'column_content'		=> _x('Content', 'tinymce popup input label', 'cocoshortcodes'),
+        
+        'column_shortcode'		=> _x('cocorico_column', 'shortcode name', 'cocoshortcodes'),
+        'column_size_att'		=> _x('size', 'shortcode attribute name', 'cocoshortcodes'),
+        'column_position_att'	=> _x('position', 'shortcode attribute name', 'cocoshortcodes'),
+        
+        // Column shortcode strings
+        
+        'message'				=> _x('Message box', 'tinymce button label', 'cocoshortcodes'),
+        'message_add'			=> _x('Add a message box', 'tinymce popup title', 'cocoshortcodes'),
+        
+        'message_type'			=> _x('Type', 'tinymce popup attribute label', 'cocoshortcodes'),
+        'message_info'			=> _x('Info', 'tinymce popup select label', 'cocoshortcodes'),
+        'message_info_value'	=> _x('info', 'shortcode attribute value', 'cocoshortcodes'),
+        'message_alert'			=> _x('Alert', 'tinymce popup select label', 'cocoshortcodes'),
+        'message_alert_value'	=> _x('alert', 'shortcode attribute value', 'cocoshortcodes'),
+        'message_error'			=> _x('Error', 'tinymce popup select label', 'cocoshortcodes'),
+        'message_error_value'	=> _x('error', 'shortcode attribute value', 'cocoshortcodes'),
+        'message_success'		=> _x('Success', 'tinymce popup select label', 'cocoshortcodes'),
+        'message_success_value'	=> _x('success', 'shortcode attribute value', 'cocoshortcodes'),
+        
+        'message_content'		=> _x('Content', 'tinymce popup input label', 'cocoshortcodes'),
+        
+        'message_shortcode'		=> _x('cocorico_message', 'shortcode name', 'cocoshortcodes'),
+        'message_type_att'		=> _x('type', 'shortcode attribute name', 'cocoshortcodes'),
+        
+        // Button shortcode strings
+        
+        'button'				=> _x('Button', 'tinymce button label', 'cocoshortcodes'),
+        'button_add'			=> _x('Add a button', 'tinymce popup title', 'cocoshortcodes'),
+        
+        'button_url'			=> _x('URL', 'tinymce popup attribute label', 'cocoshortcodes'),
+        'button_label'			=> _x('Label', 'tinymce popup attribute label', 'cocoshortcodes'),
+        
+        'button_target'			=> _x('URL Target', 'tinymce popup attribute label', 'cocoshortcodes'),
+        'button_link_same'		=> _x('Current window', 'tinymce popup select label', 'cocoshortcodes'),
+        'button_link_same_value'=> _x('current_window', 'shortcode attribute value', 'cocoshortcodes'),
+        'button_link_other'		=> _x('New window', 'tinymce popup select label', 'cocoshortcodes'),
+        'button_link_other_value'=> _x('new_window', 'shortcode attribute value', 'cocoshortcodes'),
+        
+        'button_style_label'	=> _x('Styling', 'tinymce section title', 'cocoshortcodes'),
+        
+        'button_size'			=> _x('Size', 'tinymce popup attribute label', 'cocoshortcodes'),
+        'button_small'			=> _x('Small', 'tinymce popup select label', 'cocoshortcodes'),
+        'button_small_value'	=> _x('small', 'shortcode attribute value', 'cocoshortcodes'),
+        'button_medium'			=> _x('Medium', 'tinymce popup select label', 'cocoshortcodes'),
+        'button_medium_value'	=> _x('medium', 'shortcode attribute value', 'cocoshortcodes'),
+        'button_large'			=> _x('Large', 'tinymce popup select label', 'cocoshortcodes'),
+        'button_large_value'	=> _x('large', 'shortcode attribute value', 'cocoshortcodes'),
+        
+        'button_color'			=> _x('Color', 'tinymce popup attribute label', 'cocoshortcodes'),
+        'button_grey'			=> _x('Grey', 'tinymce popup select label', 'cocoshortcodes'),
+        'button_grey_value'		=> _x('grey', 'shortcode attribute value', 'cocoshortcodes'),
+        'button_purple'			=> _x('Purple', 'tinymce popup select label', 'cocoshortcodes'),
+        'button_purple_value'	=> _x('purple', 'shortcode attribute value', 'cocoshortcodes'),
+        'button_blue'			=> _x('Blue', 'tinymce popup select label', 'cocoshortcodes'),
+        'button_blue_value'		=> _x('blue', 'shortcode attribute value', 'cocoshortcodes'),
+        'button_turquoise'		=> _x('Turquoise', 'tinymce popup select label', 'cocoshortcodes'),
+        'button_turquoise_value'=> _x('turquoise', 'shortcode attribute value', 'cocoshortcodes'),
+        'button_green'			=> _x('Green', 'tinymce popup select label', 'cocoshortcodes'),
+        'button_green_value'	=> _x('green', 'shortcode attribute value', 'cocoshortcodes'),
+        'button_yellow'			=> _x('Yellow', 'tinymce popup select label', 'cocoshortcodes'),
+        'button_yellow_value'	=> _x('yellow', 'shortcode attribute value', 'cocoshortcodes'),
+        'button_orange'			=> _x('Orange', 'tinymce popup select label', 'cocoshortcodes'),
+        'button_orange_value'	=> _x('orange', 'shortcode attribute value', 'cocoshortcodes'),
+        'button_red'			=> _x('Red', 'tinymce popup select label', 'cocoshortcodes'),
+        'button_red_value'		=> _x('red', 'shortcode attribute value', 'cocoshortcodes'),
+        'button_pink'			=> _x('Pink', 'tinymce popup select label', 'cocoshortcodes'),
+        'button_pink_value'		=> _x('pink', 'shortcode attribute value', 'cocoshortcodes'),
+        'button_nightblue'		=> _x('Night Blue', 'tinymce popup select label', 'cocoshortcodes'),
+        'button_nightblue_value'=> _x('nightblue', 'shortcode attribute value', 'cocoshortcodes'),
+        
+        'button_shortcode'		=> _x('cocorico_button', 'shortcode name', 'cocoshortcodes'),
+        'button_url_att'		=> _x('url', 'shortcode attribute name', 'cocoshortcodes'),
+        'button_target_att'		=> _x('target', 'shortcode attribute name', 'cocoshortcodes'),
+        'button_size_att'		=> _x('size', 'shortcode attribute name', 'cocoshortcodes'),
+        'button_color_att'		=> _x('color', 'shortcode attribute name', 'cocoshortcodes'),
+		
+		// Tabs shortcode strings
+		
+		'tabs'					=> _x('Tabs', 'tinymce button label', 'cocoshortcodes'),
+        'tabs_add'				=> _x('Add tabs', 'tinymce popup title', 'cocoshortcodes'),
+        
+        'tabs_label'			=> _x('Select how many tabs you want to insert', 'tinymce section title', 'cocoshortcodes'),
+        'tabs_number'			=> _x('Number', 'tinymce popup attribute label', 'cocoshortcodes'),
+        
+        'tab_label_value'		=> _x('Tab label', 'shortcode attribute value', 'cocoshortcodes'),
+        'tab_content'			=> _x('Tab content', 'shortcode default content', 'cocoshortcodes'),
+        'tab_label_att'			=> _x('label', 'shortcode attribute name', 'cocoshortcodes'),
+        
+        'tabs_shortcode'		=> _x('cocorico_tabs', 'shortcode name', 'cocoshortcodes'),
+        'single_tab_shortcode'	=> _x('cocorico_tab', 'shortcode name', 'cocoshortcodes'),
+        
+        // Separator Shortcode strings
+        
+        'separator'				=> _x('Separator', 'tinymce button label', 'cocoshortcodes'),
+        'separator_add'			=> _x('Add a separator', 'tinymce popup title', 'cocoshortcodes'),
+        'separator_style'		=> _x('Choose a style for this separator', 'tinymce popup attribute label', 'cocoshortcodes'),
+        
+        'separator_solid'		=> _x('Solid', 'tinymce popup select label', 'cocoshortcodes'),
+        'separator_solid_value'	=> _x('solid', 'shortcode attribute value', 'cocoshortcodes'),
+        'separator_dashed'		=> _x('Dashed', 'tinymce popup select label', 'cocoshortcodes'),
+        'separator_dashed_value'=> _x('dashed', 'shortcode attribute value', 'cocoshortcodes'),
+        'separator_dotted'		=> _x('Dotted', 'tinymce popup select label', 'cocoshortcodes'),
+        'separator_dotted_value'=> _x('dotted', 'shortcode attribute value', 'cocoshortcodes'),
+        'separator_double'		=> _x('Double', 'tinymce popup select label', 'cocoshortcodes'),
+        'separator_double_value'=> _x('double', 'shortcode attribute value', 'cocoshortcodes'),
+        
+        'separator_shortcode'	=> _x('cocorico_separator', 'shortcode name', 'cocoshortcodes'),
+        'separator_style_att'	=> _x('style', 'shortcode attribute name', 'cocoshortcodes'),
+        
+        // Toggle Shortcode strings
+        
+        'toggle'				=> _x('Toggle', 'tinymce button label', 'cocoshortcodes'),
+        'toggle_add'			=> _x('Add a toggle', 'tinymce popup title', 'cocoshortcodes'),
+        
+        'toggle_title'			=> _x('Title', 'tinymce popup attribute label', 'cocoshortcodes'),
+        
+        'toggle_content'		=> _x('Content', 'tinymce popup input label', 'cocoshortcodes'),
+        
+        'toggle_shortcode'		=> _x('cocorico_toggle', 'shortcode name', 'cocoshortcodes'),
+        'toggle_title_att'		=> _x('title', 'shortcode attribute name', 'cocoshortcodes'),
+        
+        // Call to action Shortcode strings
+        
+        'cta'					=> _x('Call to action', 'tinymce button label', 'cocoshortcodes'),
+        'cta_add'				=> _x('Add a call to action', 'tinymce popup title', 'cocoshortcodes'),
+        
+        'cta_title'				=> _x('Title', 'tinymce popup attribute label', 'cocoshortcodes'),
+        'cta_url'				=> _x('URL', 'tinymce popup attribute label', 'cocoshortcodes'),
+        'cta_label'				=> _x('Button\'s label', 'tinymce popup attribute label', 'cocoshortcodes'),
+        'cta_color'				=> _x('Button\'s color', 'tinymce popup attribute label', 'cocoshortcodes'),
+        
+        'cta_content'			=> _x('Content', 'tinymce popup input label', 'cocoshortcodes'),
+        
+        'cta_shortcode'			=> _x('cocorico_calltoaction', 'shortcode name', 'cocoshortcodes'),
+        'cta_title_att'			=> _x('title', 'shortcode attribute name', 'cocoshortcodes'),
+        'cta_url_att'			=> _x('url', 'shortcode attribute name', 'cocoshortcodes'),
+        'cta_label_att'			=> _x('label', 'shortcode attribute name', 'cocoshortcodes'),
+        'cta_color_att'			=> _x('color', 'shortcode attribute name', 'cocoshortcodes'),
+        
+		
+    );
+    $locale = _WP_Editors::$mce_locale;
+    $translated = 'tinyMCE.addI18n("' . $locale . '.coco_shortcodes_tinymce", ' . json_encode( $strings ) . ");\n";
+
+     return $translated;
+}
+
+$strings = coco_shortcodes_tinymce_translation();
---
+++ cocorico-shortcodes-library.php
@@ -0,0 +1,357 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// Thanks to my friend Rémi Corson for those snippets :
+// http://www.remicorson.com/how-to-create-translation-ready-shortcodes/
+
+function coco_shortcodes_register(){
+   add_shortcode(_x('cocorico_column', 'shortcode name', 'cocoshortcodes'), 'coco_shortcodes_column');
+   add_shortcode(_x('cocorico_message', 'shortcode name', 'cocoshortcodes'), 'coco_shortcodes_message');
+   add_shortcode(_x('cocorico_button', 'shortcode name', 'cocoshortcodes'), 'coco_shortcodes_button');
+   add_shortcode(_x('cocorico_tabs', 'shortcode name', 'cocoshortcodes'), 'coco_shortcodes_tabs');
+   add_shortcode(_x('cocorico_tab', 'shortcode name', 'cocoshortcodes'), 'coco_shortcodes_tab');
+   add_shortcode(_x('cocorico_separator', 'shortcode name', 'cocoshortcodes'), 'coco_shortcodes_separator');
+   add_shortcode(_x('cocorico_toggle', 'shortcode name', 'cocoshortcodes'), 'coco_shortcodes_toggle');
+   add_shortcode(_x('cocorico_calltoaction', 'shortcode name', 'cocoshortcodes'), 'coco_shortcodes_calltoaction');
+}
+add_action( 'init', 'coco_shortcodes_register');
+
+
+// Column Shortcode generator
+if(!function_exists('coco_shortcodes_column')){
+	function coco_shortcodes_column($atts, $content = null) {
+		
+		extract(shortcode_atts(array(
+			_x('size', 'shortcode attribute name', 'cocoshortcodes') => '',
+			_x('position', 'shortcode attribute name', 'cocoshortcodes') => ''
+		), $atts));
+		
+		$size = ${_x('size', 'shortcode attribute name', 'cocoshortcodes')};
+		$position = ${_x('position', 'shortcode attribute name', 'cocoshortcodes')};
+		
+		$classes = 'cs_column ';
+		
+		switch($size){
+			case _x('one_half', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= 'cs_one_half';
+			break;
+			case _x('one_third', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= 'cs_one_third';
+			break;
+			case _x('two_thirds', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= 'cs_two_thirds';
+			break;
+			case _x('one_fourth', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= 'cs_one_fourth';
+			break;
+			case _x('three_fourths', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= 'cs_three_fourths';
+			break;
+			default :
+			 	$classes .= '';
+		}
+		
+		switch($position){
+			case _x('first', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= ' cs_column_first';
+			break;
+			case _x('middle', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= ' cs_column_middle';
+			break;
+			case _x('last', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= ' cs_column_last';
+			break;
+			default:
+				$classes .= '';
+		}
+		
+		$res = '<div class="'.$classes.'">';
+		$res .= do_shortcode(wpautop($content));
+		$res .= '</div>';
+		
+		$res .= ($position == _x('last', 'shortcode attribute name', 'cocoshortcodes') ? '<div class="cs_clear"></div>' : '');
+		
+		return $res;
+	}
+}
+
+// Message Shortcode generator
+if (!function_exists('coco_shortcodes_message')){
+	function coco_shortcodes_message($atts, $content = null){
+		
+		extract(shortcode_atts(array(
+			_x('type', 'shortcode attribute name', 'cocoshortcodes') => '',
+		), $atts));
+		
+		$type = ${_x('type', 'shortcode attribute name', 'cocoshortcodes')};
+		
+		$classes = 'cs_message ';
+		
+		switch($type){
+			case _x('info', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= 'cs_message_info';
+			break;
+			case _x('alert', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= 'cs_message_alert';
+			break;
+			case _x('error', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= 'cs_message_error';
+			break;
+			case _x('success', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= 'cs_message_success';
+			break;
+			default :
+			 	$classes .= '';
+		}
+		
+		$res = '<div class="'.$classes.'">';
+		$res .= do_shortcode(wpautop($content));
+		$res .= '</div>';
+		return $res;
+	}
+}
+
+// Button Shortcode generator
+if (!function_exists('coco_shortcodes_button')){
+	function coco_shortcodes_button($atts, $content = null){
+		
+		extract(shortcode_atts(array(
+			_x('url', 'shortcode attribute name', 'cocoshortcodes') => 'https://www.themesdefrance.fr',
+			_x('target', 'shortcode attribute name', 'cocoshortcodes') => '',
+			_x('size', 'shortcode attribute name', 'cocoshortcodes') => _x('medium', 'shortcode attribute value', 'cocoshortcodes'),
+			_x('color', 'shortcode attribute name', 'cocoshortcodes') => ''
+		), $atts));
+		
+		$url 	= ${_x('url', 'shortcode attribute name', 'cocoshortcodes')};
+		$target = ${_x('target', 'shortcode attribute name', 'cocoshortcodes')};
+		$size 	= ${_x('size', 'shortcode attribute name', 'cocoshortcodes')};
+		$color 	= ${_x('color', 'shortcode attribute name', 'cocoshortcodes')};
+		
+		$target = ($target == _x('new_window', 'shortcode attribute value', 'cocoshortcodes') ? ' target="_blank" ' : '');
+		
+		$classes = 'cs_button';
+		
+		switch($size){
+			case _x('small', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= ' cs_button_small';
+			break;
+			case _x('medium', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= ' cs_button_medium';
+			break;
+			case _x('large', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= ' cs_button_large';
+			break;
+			default :
+			 	$classes .= ' cs_button_medium';
+		}
+		
+		switch($color){
+			case _x('purple', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= ' cs_button_purple';
+			break;
+			case _x('blue', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= ' cs_button_blue';
+			break;
+			case _x('turquoise', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= ' cs_button_turquoise';
+			break;
+			case _x('green', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= ' cs_button_green';
+			break;
+			case _x('yellow', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= ' cs_button_yellow';
+			break;
+			case _x('orange', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= ' cs_button_orange';
+			break;
+			case _x('red', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= ' cs_button_red';
+			break;
+			case _x('pink', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= ' cs_button_pink';
+			break;
+			case _x('grey', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= ' cs_button_grey';
+			break;
+			case _x('nightblue', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= ' cs_button_nightblue';
+			break;
+			default :
+			 	$classes .= ' cs_button_grey';
+		}
+		
+		$res = '<a href="' . $url . '" class="' . $classes . '"' . $target . '>';
+		$res .= $content;
+		$res .= '</a>';
+		
+		return $res;
+	}
+}
+
+// Tabs Shortcode generator
+if (!function_exists('coco_shortcodes_tabs')){
+	function coco_shortcodes_tabs($atts, $content = null){
+		
+		// Load jquery tabs
+		wp_enqueue_script('jquery-ui-tabs');
+		
+		// Thanks to https://stackoverflow.com/questions/23307032/create-wordpress-shortcode-for-jquery-ui-tabs
+		
+		preg_match_all( '/' . _x('cocorico_tab', 'shortcode name', 'cocoshortcodes') . ' ' . _x('label', 'shortcode attribute name', 'cocoshortcodes') . '="([^\"]+)"/i', $content, $matche, PREG_OFFSET_CAPTURE );
+
+	    $tab_title = array();
+	
+	    if( isset($matche[1]) ) {
+	        $tab_title = $matche[1];
+	    }
+	
+	    $res = '';
+	
+	    if( count($tab_title) ) {
+	        $res .= '<div class="cs_tabs">';
+	        $res .= '<ul class="nav cs_clear">';
+	        foreach( $tab_title as $tab ){
+	            $res .= '<li><a href="#cs_tabs_'. str_replace('-', '_', sanitize_title( $tab[0] )) .'">' . $tab[0] . '</a></li>';
+	        }
+	        $res .= '</ul>' . do_shortcode($content) . '</div>';
+	    } else {
+	        $res .= do_shortcode($content);
+	    }
+	
+	    return $res;
+	}
+}
+
+// Single Tab Shortcode 
+if (!function_exists('coco_shortcodes_tab')){
+	function coco_shortcodes_tab($atts, $content = null){
+	
+		extract(shortcode_atts(array(
+			_x('label', 'shortcode attribute name', 'cocoshortcodes') => _x('Tab label', 'shortcode attribute value', 'cocoshortcodes'),
+		), $atts));
+		
+		$label = ${_x('label', 'shortcode attribute name', 'cocoshortcodes')};
+		
+		$res = '<div class="cs_tab_content" id="cs_tabs_'. str_replace('-', '_', sanitize_title( $label )) .'">'. do_shortcode(wpautop($content)) .'</div>';
+		
+		return $res;
+	}
+}
+
+// Separator Shortcode (hr)
+if (!function_exists('coco_shortcodes_separator')){
+	function coco_shortcodes_separator($atts) {
+		
+		extract(shortcode_atts(array(
+			_x('style', 'shortcode attribute name', 'cocoshortcodes') => _x('solid', 'shortcode attribute value', 'cocoshortcodes'),
+		), $atts));
+		
+		$style = ${_x('style', 'shortcode attribute name', 'cocoshortcodes')};
+		
+		$class = '';
+		
+		switch($style){
+			case _x('solid', 'shortcode attribute value', 'cocoshortcodes') :
+				$class .= 'cs_separator_solid';
+			break;
+			case _x('dashed', 'shortcode attribute value', 'cocoshortcodes') :
+				$class .= 'cs_separator_dashed';
+			break;
+			case _x('dotted', 'shortcode attribute value', 'cocoshortcodes') :
+				$class .= 'cs_separator_dotted';
+			break;
+			case _x('double', 'shortcode attribute value', 'cocoshortcodes') :
+				$class .= 'cs_separator_double';
+			break;
+			default :
+			 	$class .= 'cs_separator_solid';
+		}
+
+		$res = '<div class="cs_separator cs_clear"><hr class="'.$class.'"></div>';
+		
+	   return $res;
+	}
+}
+
+// Toggle Shortcode Generator
+if (!function_exists('coco_shortcodes_toggle')){
+	function coco_shortcodes_toggle($atts, $content = null) {
+		
+		extract(shortcode_atts(array(
+			_x('title', 'shortcode attribute name', 'cocoshortcodes') => _x('Toggle title', 'shortcode attribute value', 'cocoshortcodes'),
+		), $atts));
+		
+		$title = ${_x('title', 'shortcode attribute name', 'cocoshortcodes')};
+		
+		$res = '<div class="cs_toggle">';
+		$res .= '<p class="cs_toggle_button"><a href="#">'.$title.'</a></p>';
+		$res .= '<div class="cs_toggle_content">' . do_shortcode(wpautop($content)) . '</div>';
+		$res .= '</div>';
+		
+	   return $res;
+	}
+}
+
+// Call to action Shortcode Generator
+if (!function_exists('coco_shortcodes_calltoaction')){
+	function coco_shortcodes_calltoaction($atts, $content = null) {
+		
+		extract(shortcode_atts(array(
+			_x('title', 'shortcode attribute name', 'cocoshortcodes') => '',
+			_x('url', 'shortcode attribute name', 'cocoshortcodes') => '',
+			_x('label', 'shortcode attribute name', 'cocoshortcodes') => '',
+			_x('color', 'shortcode attribute name', 'cocoshortcodes') => '',
+		), $atts));
+		
+		$title = ${_x('title', 'shortcode attribute name', 'cocoshortcodes')};
+		$url = ${_x('url', 'shortcode attribute name', 'cocoshortcodes')};
+		$label = ${_x('label', 'shortcode attribute name', 'cocoshortcodes')};
+		$color = ${_x('color', 'shortcode attribute name', 'cocoshortcodes')};
+		
+		$classes = 'cs_button cs_button_calltoaction';
+		
+		switch($color){
+			case _x('purple', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= ' cs_button_purple';
+			break;
+			case _x('blue', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= ' cs_button_blue';
+			break;
+			case _x('turquoise', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= ' cs_button_turquoise';
+			break;
+			case _x('green', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= ' cs_button_green';
+			break;
+			case _x('yellow', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= ' cs_button_yellow';
+			break;
+			case _x('orange', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= ' cs_button_orange';
+			break;
+			case _x('red', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= ' cs_button_red';
+			break;
+			case _x('pink', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= ' cs_button_pink';
+			break;
+			case _x('grey', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= ' cs_button_grey';
+			break;
+			case _x('nightblue', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= ' cs_button_nightblue';
+			break;
+			default :
+			 	$classes .= ' cs_button_grey';
+		}
+		
+		$res = '<div class="cs_calltoaction">';
+		$res .= '<h3 class="cs_calltoaction_title">' . $title . '</h3>';
+		$res .= '<p class="cs_calltoaction_content">' . do_shortcode($content) . '</p>';
+		$res .= '<a href="' . $url . '" class="' . $classes . '">' . $label . '</a>';
+		$res .= '</div>';
+		
+	   return $res;
+	}
+}
